96f8e5b748f2b959afd9da0b28689a365eeb7231
Sergey <bindasov.sergey@gmail.com>
2021-07-08 13:09:04 +0300

    no message

--- Navigation/Profile/PhotosViewController.swift
+++ Navigation/Profile/PhotosViewController.swift
@@ -8,30 +8,54 @@
 
 import UIKit
 import StorageService
+import iOSIntPackage
 
-class PhotosViewController: UIViewController {
+class PhotosViewController: UIViewController, ImageLibrarySubscriber {
+  
+    var imageArray: [UIImage] = []
+    
+    var imagePublisher: ImagePublisherFacade? = .init()
     
     private lazy var galleryCollection: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
-      
         let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collection.register(PhotosCollectionViewCell.self, forCellWithReuseIdentifier: String(describing: PhotosCollectionViewCell.self))
         collection.delegate = self
         collection.dataSource = self
         collection.backgroundColor = .white
         collection.showsVerticalScrollIndicator = false
-    
         collection.toAutoLayuot()
         
         return collection
     }()
     
+    func receive(images: [UIImage]) {
+        self.imageArray = images
+        self.galleryCollection.reloadData()
+    }
+    
+    func converter(images: [Pic]) -> [UIImage] {
+        var newArray: [UIImage] = []
+        for image in images {
+            guard let photo = image.image else { return [] }
+            newArray.append(photo)
+        }
+        return newArray
+    }
+        
     override func viewDidLoad() {
         super .viewDidLoad()
         setupLayout()
         view.backgroundColor = .white
         navigationController?.navigationBar.isHidden = false
         navigationItem.title = "Photo Galley"
+        imagePublisher?.subscribe(self)
+        imagePublisher?.addImagesWithTimer(time: 0.5, repeat: 20, userImages: converter(images: Storage.Photos.photos))
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(true)
+        imagePublisher?.removeSubscription(for: self)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -39,16 +63,19 @@ class PhotosViewController: UIViewController {
     }
 }
 
+
+
 extension PhotosViewController: UICollectionViewDataSource {
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        let items = Storage.Photos.photos.count
+        let items = imageArray.count
         return items
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: PhotosCollectionViewCell.self), for: indexPath) as! PhotosCollectionViewCell
-        cell.gallery = Storage.Photos.photos[indexPath.item]
         
+        cell.gallery = imageArray[indexPath.item]
         return cell
     }
     
--- StorageService/Storage.swift
+++ StorageService/Storage.swift
@@ -23,7 +23,6 @@ public class Storage {
     public struct Photos {
         public static var photos = [Pic(image: UIImage(named: "01")), Pic(image: UIImage(named: "02")), Pic(image: UIImage(named: "03")), Pic(image: UIImage(named: "04")), Pic(image: UIImage(named: "05")), Pic(image: UIImage(named: "06")), Pic(image: UIImage(named: "07")), Pic(image: UIImage(named: "08")), Pic(image: UIImage(named: "09")), Pic(image: UIImage(named: "10")), Pic(image: UIImage(named: "11")), Pic(image: UIImage(named: "12")), Pic(image: UIImage(named: "13")), Pic(image: UIImage(named: "14")), Pic(image: UIImage(named: "15")), Pic(image: UIImage(named: "16")), Pic(image: UIImage(named: "17")), Pic(image: UIImage(named: "18")), Pic(image: UIImage(named: "19")), Pic(image: UIImage(named: "20"))]
     }
-
     
 }
 
