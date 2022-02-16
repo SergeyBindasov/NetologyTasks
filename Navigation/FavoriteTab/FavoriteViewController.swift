d4466000c924410e914019edef6a33dee495bdbf
Sergey <bindasov.sergey@gmail.com>
2022-02-16 15:27:25 +0300

    Добавил таблицу для отображения постов и модель постов в качестве ячейки

--- Navigation/FavoriteTab/FavoriteViewController.swift
+++ Navigation/FavoriteTab/FavoriteViewController.swift
@@ -7,13 +7,55 @@
 //
 
 import UIKit
+import SnapKit
+import StorageService
 
 class FavoriteViewController: UIViewController {
     
     weak var coordinator: FavoriteFlowCoordinator?
     
+    var favoritePost: [Post] = []
+    
+    private lazy var favTavleView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(PostTableViewCell.self, forCellReuseIdentifier: String(describing: PostTableViewCell.self))
+        tableView.dataSource = self
+        tableView.delegate = self
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        setupLayout()
     }
 }
+
+extension FavoriteViewController: UITableViewDelegate {
+    
+}
+
+extension FavoriteViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favoritePost.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: PostTableViewCell.self), for: indexPath) as? PostTableViewCell
+        guard let newCell = cell else { return PostTableViewCell()}
+        return newCell
+    }
+}
+
+extension FavoriteViewController {
+    func setupLayout() {
+        view.addSubviews(favTavleView)
+        
+        favTavleView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaInsets)
+        }
+    }
+}
+
+
+
